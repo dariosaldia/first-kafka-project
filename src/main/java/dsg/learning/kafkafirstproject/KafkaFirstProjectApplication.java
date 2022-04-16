4ff60f9fabdf7d5090a0451ac490d2e139dc5f78
Dario Saldia <dsaldia@temperies.com>
2022-04-16 20:31:10 +0200

    Initial commit

---
+++ src/main/java/dsg/learning/kafkafirstproject/KafkaFirstProjectApplication.java
@@ -0,0 +1,13 @@
+package dsg.learning.kafkafirstproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaFirstProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaFirstProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dsg/learning/kafkafirstproject/KafkaFirstProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package dsg.learning.kafkafirstproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaFirstProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
